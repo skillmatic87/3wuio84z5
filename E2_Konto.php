edfcf04bf5230e2a16457636a0f0a5046711fa51
Michael Hoffmann <cleptric@users.noreply.github.com>
2019-06-08 20:59:48 +0200

    Update E2_Konto.php

--- E2_Konto.php
+++ E2_Konto.php
@@ -1,27 +1,26 @@
 <?php
 require 'konto.class.php';
 
-    function abbuchen(int $Betrag) {
-        if ($this->Kontostand - $Betrag <0) {
-            echo "Abzubuchender Betrag überschreitet Kontostand, Buchung wurde nicht ausgeführt!";
-        }
-                else {
-                    $this->Kontostand = $this->Kontostand - $Betrag;
-                    echo "Betrag: $Betrag wurde abgehoben. Neuer Kontostand: $this->Kontostand";
-                }
+function abbuchen(int $Betrag, Konto $konto) {
+    if ($konto->Kontostand - $Betrag <0) {
+        echo "Abzubuchender Betrag überschreitet Kontostand, Buchung wurde nicht ausgeführt!";
+    } else {
+        $konto->Kontostand = $konto->Kontostand - $Betrag;
+        echo "Betrag: $Betrag wurde abgehoben. Neuer Kontostand: $konto->Kontostand";
     }
-    function einzahlen(int $Betrag) {
-        $this->Kontostand = $this->Kontostand + $Betrag;
-        echo "Betrag: $Betrag wurde eingezahlt. Neuer Kontostand: $this->Kontostand";
-    }
+}
+function einzahlen(int $Betrag, Konto $konto) {
+    $konto->Kontostand = $konto->Kontostand + $Betrag;
+    echo "Betrag: $Betrag wurde eingezahlt. Neuer Kontostand: $konto->Kontostand";
+}
 
 $Konto1 = new Konto(123,5000,'Lina Petzold');
 $Konto2 = new Konto(456,1250,'Karsten Schlütter');
 
-abbuchen(500)->$Konto1;
-einzahlen(1000)->$Konto2;
-einzahlen(250)->$Konto1;
-abbuchen(10000)->$Konto2;
+abbuchen(500, $Konto1);
+einzahlen(1000, $Konto2);
+einzahlen(250, $Konto1);
+abbuchen(10000, $Konto2);
 
     
 ?>
